76f452460c893b8cc8b31ff991df70d01414bcd6
Tiago Costa <tiagodpv@gmail.com>
2023-05-17 14:42:00 -0400

    add grade

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = int(input("what is x? "))
+y = int(input("what is y? "))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal y")
