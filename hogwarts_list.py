f5e0d84227c4a0119f66c187ab06baff512ebddd
Tiago Costa <tiagodpv@gmail.com>
2023-05-19 14:53:19 -0400

    add list

---
+++ hogwarts_list.py
@@ -0,0 +1,10 @@
+students = ["Hermione", "Harry", "Ron"]
+
+
+for i in range(len(students)):
+    print(i+1, students[i])
+
+print("\n")
+
+for students in students:
+    print(students)
--- loop_cat.py
+++ loop_cat.py
@@ -1,1 +1,7 @@
-print("meow\n" * 3, end="")
+while True:
+    n = int(input("what's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
