a086b917dae46a21e8a83b3b9bff767a60842c39
Tiago Costa <tiagodpv@gmail.com>
2023-05-22 18:00:32 -0400

    add random

---
+++ generate_random.py
@@ -0,0 +1,12 @@
+# from random import choice
+
+# coin = choice(["heads", "tails"])
+# print(coin)
+
+
+import random
+
+cards = ["jack", "queen",  "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
