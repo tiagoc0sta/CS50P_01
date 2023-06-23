8abdbd3c6ba6cc8072f63674b7623f3dc7c90a6f
Tiago Costa <tiagodpv@gmail.com>
2023-06-23 19:14:51 -0400

    add students

--- students.py
+++ students.py
@@ -1,9 +1,23 @@
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Gryffindor",
-}
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor",
+#     "Draco": "Gryffindor",
+# }
 
-for student in students:
-    print(student)
+# for student in students:
+#     print(student)
+
+students = []
+
+with open("studens.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+        def get_name(student):
+            return student["name"]
+
+        for student in sorted(students):
+            print(f"{student['name']} is in {student['house']}")
