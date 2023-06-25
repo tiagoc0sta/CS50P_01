90ffbecc3a8f16c17a5c8bd7512cd9c53d6c6d50
Tiago Costa <tiagodpv@gmail.com>
2023-06-25 11:25:17 -0400

    change sstd

--- students.py
+++ students.py
@@ -1,23 +1,23 @@
-# students = {
-#     "Hermione": "Gryffindor",
-#     "Harry": "Gryffindor",
-#     "Ron": "Gryffindor",
-#     "Draco": "Gryffindor",
-# }
+ students = {
+     "Hermione": "Gryffindor",
+     "Harry": "Gryffindor",
+     "Ron": "Gryffindor",
+     "Draco": "Gryffindor",
+ }
 
 # for student in students:
 #     print(student)
 
-students = []
+# students = []
 
-with open("studens.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+# with open("studens.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
 
-        def get_name(student):
-            return student["name"]
+#         def get_name(student):
+#             return student["name"]
 
-        for student in sorted(students, key=get_name):
-            print(f"{student['name']} is in {student['house']}")
+#         for student in sorted(students, key=get_name):
+#             print(f"{student['name']} is in {student['house']}")
